4c5d32c87ecf070a1b85aadcf4e3f88754c22297
webdbmike <webdbmike@users.noreply.github.com>
2015-07-06 22:31:15 -0500

    Initial drop of project

---
+++ gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: ["./public/js/**/*.js"],
+      options: {
+        unused: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  grunt.registerInitTask("default",["jshint"]);
+
+};
